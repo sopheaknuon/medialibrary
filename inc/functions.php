b6e8031567a061d1837d784c9b1bd892d912f687
Sagar M <sagardoesweb@gmail.com>
2016-04-15 09:39:44 +0530

    Display categories

--- inc/functions.php
+++ inc/functions.php
@@ -7,3 +7,18 @@ function get_item_html($id,$item) {
         . "</a></li>";
     return $output;
 }
+
+function array_category($catalog,$category) {
+    if ($category === null) {
+        return array_keys($catalog);
+    }
+    $output = array();
+    
+    foreach ($catalog as $id => $item) {
+        if (strtolower($category) == strtolower($item["category"])) {
+            $output[] = $id;            
+        }
+    }
+    
+    return $output;
+}
