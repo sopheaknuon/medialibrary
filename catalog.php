3ad08a1ae4f7c477605cbd60fd61769815a585b6
Sagar M <sagardoesweb@gmail.com>
2016-04-15 09:33:57 +0530

    Add page titles

--- catalog.php
+++ catalog.php
@@ -1,7 +1,20 @@
-<?php include("inc/header.php"); ?>
+<?php 
+$pageTitle = "Full Catalog";
 
-<div class="section page">
-    <h1>Full Catalog</h1>
+if (isset($_GET["cat"])) {
+    if ($_GET["cat"] == "books") {
+        $pageTitle = "Books";
+    } else if ($_GET["cat"] == "movies") {
+        $pageTitle = "Movies";
+    } else if ($_GET["cat"] == "music") {
+        $pageTitle = "Music";
+    }
+}
+
+include("inc/header.php"); ?>
+
+<div class="section catalog page">
+    <h1><?php echo $pageTitle; ?></h1>
 </div>
 
 <?php include("inc/footer.php"); ?>
