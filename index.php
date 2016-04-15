5421bd8351dccc43829ea31812d8c0ee372d98d0
Sagar M <sagardoesweb@gmail.com>
2016-04-15 09:28:51 +0530

    Add header

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php include("inc/header.php"); ?>
+		<div class="section catalog random">
+
+			<div class="wrapper">
+
+				<h2>May we suggest something?</h2>
+
+        <ul class="items">
+            <li>
+                <a href="#">
+                    <img src="img/media/forest_gump.jpg" alt="Forrest Gump">
+                    <p>View Details</p>
+                </a>
+            </li><li>
+                <a href="#">
+                    <img src="img/media/princess_bride.jpg" alt="The Princess Bride">
+                    <p>View Details</p>
+                </a>
+            </li><li>
+                <a href="#">
+                    <img src="img/media/elvis_presley.jpg" alt="Elvis Forever">
+                    <p>View Details</p>
+                </a>
+            </li><li>
+                <a href="#">
+                    <img src="img/media/garth_brooks.jpg" alt="No Fences">
+                    <p>View Details</p>
+                </a>
+            </li>								
+				</ul>
+
+			</div>
+
+		</div>
+
+	</div><!-- end content -->
+
+	<div class="footer">
+
+		<div class="wrapper">
+
+			<ul>		
+				<li><a href="http://twitter.com/treehouse">Twitter</a></li>
+				<li><a href="https://www.facebook.com/TeamTreehouse">Facebook</a></li>
+			</ul>
+
+			<p>&copy;<?php echo date("Y"); ?> Personal Media Library</p>
+
+		</div>
+	
+	</div>
+
+</body>
+</html>
